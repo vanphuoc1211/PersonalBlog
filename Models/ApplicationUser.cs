69d55e9050b96f65177f2d528298de16c12ab742
vanph <vanph@LAPTOP-GOLSRSFP>
2023-04-05 23:48:27 +0700

    Add project files.

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+namespace PersonalBlog.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set;}
+        public string? LastName { get; set;}
+        public List<Post> Posts { get; set;}
+    }
+}
